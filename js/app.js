a6665ac9f20063313a54e30c9feb180456bbcf51
Alfredo Miranda <alfredo@miranda.io>
2015-07-25 17:45:06 -0500

    little bites at a time

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 'use strict';
 
 angular.module('docOnDemand', [])
-  .controller('MainController', ['$scope', function($scope) {
-    $scope.greeting = 'Doc On Demand';
+  .controller('MainController', ['$scope', '$http', function($scope, $http) {
+    $http.get('/customfields')
+      .success(function(data, status, headers, config) {
+        console.log(data);
+      })
+      .error(function(data, status, headers, config) {
+        console.log(data);
+      });
   }]);
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var athenahealthapi = require('./athenahealthapi');
+var events = require('events');
 var app = express();
 var port = process.env.PORT || 8080;
 
@@ -6,6 +8,29 @@ app.use(express.static('views'));
 app.use(express.static('bower_components'));
 app.use(express.static('js'));
 
-app.listen(port, function() {
-  console.log('The app is running!');
-});
+var key = 'w7vh4qegxmh9xbrhkyx2b779';
+var secret = 'cNy7r8DhDJVRFWk';
+var version = 'preview1';
+var practiceid = 195900;
+var api = new athenahealthapi.Connection(version, key, secret, practiceid);
+
+api.status.on('ready', main)
+api.status.on('error', function(error) {
+          console.log(error)
+})
+
+function main() {
+
+  app.get('/customfields', function(req, res) {
+
+    api.GET('/customfields')
+      .on('done', function(response) {
+        res.json(response);
+      })
+      .on('error', function(error) {
+        console.log(error);
+      });
+  });
+
+  app.listen(port);
+}
