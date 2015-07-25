361cdf96f841ff3a18117e4d1d53a317bd8ae7cb
Alfredo Miranda <alfredo@miranda.io>
2015-07-25 15:30:23 -0500

    Serve index.html

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var app = express();
-
 var port = process.env.PORT || 8080;
 
-app.get('/', function(req, res) {
-  res.send('This is Doc on Demand!');
-});
+app.use(express.static('views'));
+app.use(express.static('bower_components'));
 
 app.listen(port, function() {
   console.log('The app is running!');
