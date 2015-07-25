eac394bceb36ac1a2866e9f027542b2638fbdfa5
Alfredo Miranda <alfredo@miranda.io>
2015-07-25 15:47:46 -0500

    make express handle static js files

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var port = process.env.PORT || 8080;
 
 app.use(express.static('views'));
 app.use(express.static('bower_components'));
+app.use(express.static('js'));
 
 app.listen(port, function() {
   console.log('The app is running!');
