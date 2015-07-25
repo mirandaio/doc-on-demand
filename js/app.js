8186fa31482fbaae527f9912e6448594d9471e84
Alfredo Miranda <alfredo@miranda.io>
2015-07-25 18:22:03 -0500

    Add angular material dependency

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('docOnDemand', [])
+angular.module('docOnDemand', ['ngMaterial'])
   .controller('MainController', ['$scope', '$http', function($scope, $http) {
     $http.get('/customfields')
       .success(function(data, status, headers, config) {
